f5df34cfc9daee0d01cbbc6c62bf2c58c571073a
deaddude <84187997+MissingReturnStatement@users.noreply.github.com>
2024-10-26 01:06:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function left() {
+    document.querySelector('.cat').style.width = "100%";
+    document.querySelector('.dog').style.width = "30px";
+}
+function both() {
+    document.querySelector('.cat').style.width = "50%";
+    document.querySelector('.dog').style.width = "50%";
+}
+function right() {
+    document.querySelector('.dog').style.width = "100%";
+    document.querySelector('.cat').style.width = "30px";
+}
